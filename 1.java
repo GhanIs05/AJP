efd65ca2ccc2995714e2833f70c7a15633c1bcce
GhanIs05 <154982500+GhanIs05@users.noreply.github.com>
2024-06-13 21:55:03 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,187 +1,211 @@
-import java.util.Scanner;
 import java.io.*;
-
-interface MySTack {
-    public void push();
+interface Mystack
+{
     public void pop();
+    public void push();
     public void display();
 }
-
-class StackArray implements MySTack{
-    final static int n = 5;
-    int stack[] = new int[n];
-    int top = -1;
-
-    public void push() {
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            if(top == (n - 1)) {
-                System.out.println("overflow");
+class Stack_array implements Mystack
+{
+    final static int n=5;
+    int stack[]=new int[n];
+    int top=-1;
+    public void push()
+    {
+        try
+        {
+            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+            if(top==(n-1))
+            {
+                System.out.println(" Stack Overflow");
                 return;
-            } else {
-                int ele;
-                System.out.print("Enter element: ");
-                ele = Integer.parseInt(br.nextLine());
-                stack[++top] = ele;
             }
-        } catch(IOException e) {
-            e.printStackTrace();
+            else
+            {
+                System.out.println("Enter the element");
+                int ele=Integer.parseInt(br.readLine());
+                stack[++top]=ele;
+            }
+        }
+        catch(IOException e)
+        {
+            System.out.println("e");
         }
     }
-
-    public void pop() {
-        if(top < 0) {
-            System.out.println("underflow");
+    public void pop()
+    {
+        if(top<0)
+        {
+            System.out.println("Stack underflow");
             return;
-        } else {
-            int popped = stack[top];
+        }
+        else
+        {
+            int popper=stack[top];
             top--;
-            System.out.println("Popped element: "+popped);
+            System.out.println("Popped element:" +popper);
         }
     }
-
-
-    public void display() {
-        if(top < 0) {
-            System.out.println("underflow");
+ 
+    public void display()
+    {
+        if(top<0)
+        {
+            System.out.println("Stack is empty");
             return;
-        } else {
-            String str = "";
-            for(int i = 0; i<= top; i++) {
-                str = str + " " + stack[i] + "<--";
-            }
-            System.out.println("Elements are: "+str);
+        }
+        else
+        {
+            String str=" ";
+            for(int i=0; i<=top; i++)
+                str=str+"  "+stack[i]+" <--";
+            System.out.println("Elements are:"+str);
         }
     }
 }
-
-class Link {
+class Link
+{
     public int data;
     public Link nextLink;
-    
-    public Link(int d) {
-        data = d;
-        nextLink = null;
+    public Link(int d)
+    {
+        data= d;
+        nextLink=null;
     }
-    
-    public void printLink() {
-        System.out.print("-->"+data);
+    public void printLink()
+    {
+        System.out.print(" --> "+data);
     }
 }
-
-class StackLink {
+class Stack_List  implements Mystack
+{
     private Link first;
-    
-    public StackLink() {
+    public Stack_List()
+    {
         first = null;
     }
-    
-    public boolean isEmpty() {
+    public boolean isEmpty()
+    {
         return first == null;
     }
-    
-    public void push() {
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("Enter element: ");
-            int ele = Integer.parseInt(br.readLine());
+    public void push()
+    {
+        try
+        {
+            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter the element");
+            int ele=Integer.parseInt(br.readLine());
             Link link = new Link(ele);
             link.nextLink = first;
             first = link;
-            
-        } catch(IOException e) {
-            e.printStackTrace();
+        }
+        catch(IOException e)
+        {
+            System.err.println(e);
         }
     }
-    
-    public Link delete() {
+    public Link delete()
+    {
         Link temp = first;
-        try {
-            
+        try
+        {
             first = first.nextLink;
-        } catch(NullPointerException e) {
-            e.printStackTrace();
+        }
+        catch(NullPointerException e)
+        {
+            throw e;
         }
         return temp;
     }
-    public void pop() {
-        try {
+    public void pop()
+    {
+        try
+        {
             Link deletedLink = delete();
-            System.out.println("Deleted element is "+deletedLink.data);
-        } catch(NullPointerException e) {
-            e.printStackTrace();
+            System.out.println("Popped: "+deletedLink.data);
+        }
+        catch(NullPointerException e)
+        {
+            throw e;
         }
     }
-    
-    public void display() {
-        if(first == null) {
-            System.out.println("Empty");
-        } else {
+    public void display()
+    {
+        if(first==null)
+            System.out.println("Stack is empty");
+        else
+        {
             Link currentLink = first;
-            System.out.print("Elements are:");
-            while(currentLink != null) {
+            System.out.print("Elements are: ");
+            while(currentLink != null)
+            {
                 currentLink.printLink();
                 currentLink = currentLink.nextLink;
             }
-            System.out.println();
+            System.out.println("");
         }
     }
 }
-
-
-
-class Main {
-    public static void main(String args[]) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
-        System.out.println("Implementation of stack using Array");
-        StackArray stackArray = new StackArray();
-        int ch = 0;
-
-        do{
-            System.out.println("1.Push 2.Pop 3.Display 4.Exit 5.Use LinkedList");
-            System.out.println("Enter yoyr choice:");
-            ch = Integer.parseInt(br.readLine());
-            switch (ch) {
-                case 1:
-                    stackArray.push();
-                    break;
-                case 2:
-                    stackArray.pop();
-                    break;
-                case 3:
-                    stackArray.display();
-                    break;
-                case 4:
-                    System.exit(0);
-                default:
-                    break;
+class StackADT
+{
+    public static void main(String arg[])throws IOException
+    {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Implementation of Stack using Array");
+        Stack_array stk=new Stack_array();
+        int ch=0;
+        do
+        {
+            System.out.println("1.Push 2.Pop 3.Display 4.Exit 5.Use Linked List");
+            System.out.println("Enter your choice:");
+            ch=Integer.parseInt(br.readLine());
+            switch(ch)
+            {
+            case 1:
+                stk.push();
+                break;
+            case 2:
+                stk.pop();
+                break;
+            case 3:
+                stk.display();
+                break;
+            case 4:
+                System.exit(0);
             }
-
-        } while(ch < 5);
-        System.out.println("Implementation of stack using Array");
-        StackLink link = new StackLink();
-        ch = 0;
-         do{
+        }
+        while(ch<5);
+        System.out.println("Implementation of Stack using Linked List");
+        Stack_List stk1=new Stack_List();
+        ch=0;
+        do
+        {
             System.out.println("1.Push 2.Pop 3.Display 4.Exit");
-            System.out.println("Enter yoyr choice:");
-            ch = Integer.parseInt(br.readLine());
-            switch (ch) {
-                case 1:
-                    link.push();
-                    break;
-                case 2:
-                    link.pop();
-                    break;
-                case 3:
-                    link.display();
-                    break;
-                case 4:
-                    System.exit(0);
-                default:
-                    break;
+            System.out.println("Enter your choice:");
+            ch=Integer.parseInt(br.readLine());
+            switch(ch)
+            {
+            case 1:
+                stk1.push();
+                break;
+            case 2:
+                try
+                {
+                    stk1.pop();
+                }
+                catch(NullPointerException e)
+                {
+                    System.out.println("Stack underflown");
+                }
+                break;
+            case 3:
+                stk1.display();
+                break;
+            default:
+                System.exit(0);
+                 
             }
-
-    }while(ch < 5);   
-    
-}
+        }
+        while(ch<5);
+    }
 }
